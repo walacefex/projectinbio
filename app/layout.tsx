3da8cdd3caa842731c678e7e73ce056369a6f103
walacefex <walacefex01@gmail.com>
2025-01-18 16:20:45 -0300

    feat: config fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
 
+import { Red_Hat_Display } from "next/font/google";
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -7,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`antialiased`}>{children}</body>
+      <body
+        className={`${redHatDisplay.className} bg-background-primary text-content-body antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
