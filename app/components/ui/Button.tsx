ca9fc22d059f080192ffc56c25e08836620ca2f2
walacefex <walacefex01@gmail.com>
2025-01-18 16:19:25 -0300

    feat: creating component button

---
+++ app/components/ui/Button.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { cn } from "../lib/utils";
+
+export default function Button({
+  children,
+  variant = "primary",
+  ...props
+}: {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary" | "ghost";
+} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      className={cn(
+        "p-3 text-white rounded-xl font-bold whitespace-nowrap hover:opacity-95 disabled:opacity-70",
+        variant === "primary" && "bg-accent-purple",
+        variant === "secondary" && "bg-background-tertiary",
+        variant === "ghost" && "border-border-primary",
+        props.className
+      )}
+      {...props}>
+      {children}
+    </button>
+  );
+}
