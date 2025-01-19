0525cc9db725ec141821ec2774e0ab8dcefc0438
walacefex <walacefex01@gmail.com>
2025-01-19 15:52:35 -0300

    feat: creating components ui

---
+++ app/components/ui/TextInput.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/app/components/lib/utils";
+export default function TextInput(
+  props: React.InputHTMLAttributes<HTMLInputElement>
+) {
+  return (
+    <input
+      {...props}
+      className={cn(
+        `w-full p-3 bg-background-secondary text-white placeholder:text-content-placeholder rounded-xl 
+        border border-transparent hover:border-border-secondary hover:text-content-body active:border-border-tertiary`,
+        props.className
+      )}
+    />
+  );
+}
