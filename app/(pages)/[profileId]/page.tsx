7a6bbc9fcbe8bcf7bff55e363c556765c5e22ece
walacefex <walacefex01@gmail.com>
2025-01-28 01:50:48 -0300

    feat: finally pages upgrade and creating profiles

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -2,19 +2,23 @@ import ProjectCard from "@/app/components/commons/ProjectCard";
 import TotalVisits from "@/app/components/commons/TotalVisits";
 import UserCard from "@/app/components/commons/UserCard";
 import { Plus } from "lucide-react";
+import Link from "next/link";
 export default async function ProfilePage({
   params,
 }: {
   params: { profileId: string };
 }) {
   const { profileId } = await params;
+
   return (
     <div className="relative h-screen flex p-20 overflow-hidden">
       <div className="fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-tertiary">
         <span>Você está usando a versão trial.</span>
-        <button className="text-accent-green font-bold">
-          Faça o upgrade agora!
-        </button>
+        <Link href={`/${profileId}/upgrade`}>
+          <button className="text-accent-green font-bold">
+            Faça o upgrade agora!
+          </button>
+        </Link>
       </div>
       <div className="w-1/2 flex justify-center h-min">
         <UserCard />
---
+++ app/(pages)/[profileId]/upgrade/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/app/components/landingPage/Header";
+import Button from "@/app/components/ui/Button";
+export default async function UpgradePage() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-4">
+      <Header />
+      <h2 className="text-2xl font-bold">Escolha o plano</h2>
+      <div className="flex gap-4">
+        <Button>R$ 9,90 / mês</Button>
+        <Button>R$ 99,90 Vitalício</Button>
+      </div>
+    </div>
+  );
+}
